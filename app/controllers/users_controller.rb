dd3b1d4c56bbbce9aa10e69fb5c51b598735dc0b
Garlandicus <ryan.darge@gmail.com>
2014-10-03 19:37:58 -0400

    Halfway through chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
 
   def new
   	@user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -18,8 +25,31 @@ class UsersController < ApplicationController
   	end
   end
 
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile gained a new level!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    # before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "You gotta sign in to see that."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
